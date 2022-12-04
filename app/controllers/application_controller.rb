bd3ba7b50ce48c04542ca98b431c5591b8718d60
Bobby Hinton <bob.g.hint@gmail.com>
2022-12-03 18:39:36 -0600

    Add more unique routes, update post/patch routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,71 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/restaurants" do
+    restaurants = Restaurant.all
+    restaurants.to_json(include: :foods)
+  end
+
+  post "/restaurants" do
+    restaurant = Restaurant.create(
+      name: params[:name],
+      city: params[:city],
+      state: params[:state],
+      food_type: params[:food_type],
+      phone_number: params[:phone_number],
+      website: params[:website],
+      online_ordering: params[:online_ordering],
+      image: params[:image]
+    )
+
+    restaurant.to_json()
+  end
+
+  # patch "/restaurants/:id" do
+  #   restaurant= Restaurant.find(params[:id])
+  #   restaurant.update(
+  #     food_type: params[:food_type],
+  #     website: params[:website],
+  #     online_ordering: params[:online_ordering]
+  #   )
+  #   restaurant.to_json
+  # end
+
+  get "/foods" do
+    foods = Food.all
+    foods.to_json(include: :restaurant)
+  end
+
+  post "/foods" do
+    food = Food.create(
+      food_name: params[:food_name],
+      restaurant_id: params[:restaurant_id],
+      price: params[:price],
+      description: params[:description],
+      image: params[:image]
+    )
+
+    food.to_json
+  end
+
+  patch "/foods/:id" do 
+    food = Food.find(params[:id])
+
+    food.update(
+      price: params[:price]
+    )
+    food.to_json
+  end
+
+  delete "/foods/:id" do
+    food = Food.find(params[:id])
+    food.destroy
+    food.to_json
+  end
+
+  get "/last_five_foods_submitted" do 
+    last_foods = Food.order(created_at: :desc).first(5)
+    last_foods.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding Restaurants, foods..."
 
 Restaurant.create(
   name: "Thai Thai Asian Bistro", 
   city: "Norman", 
   state: "OK", 
   food_type: "Thai", 
-  phone: 4053102026, 
-  website: "https://www.thaithainorman.com/#/"
+  phone_number: 4053102026, 
+  website: "https://www.thaithainorman.com/#/",
   online_ordering: true,
   image: "https://www.visitnorman.com/uploads/Thai-Thai-Asian-Bistro.jpg"
 )
@@ -16,7 +16,7 @@ Restaurant.create(
   city: "Norman",
   state: "OK",
   food_type: "Italian",
-  phone: 4052924777,
+  phone_number: 4052924777,
   website: "https://www.hideawaypizza.com/",
   online_ordering: true,
   image: "https://images.squarespace-cdn.com/content/v1/5cbf5a350cf57df7fb43982e/1576701466298-IZJRLUGZ6S8HQHXEJFUZ/OKC+Sign+Small.jpg"
@@ -27,7 +27,7 @@ Restaurant.create(
   city: "Oklahoma City",
   state: "OK",
   food_type: "German",
-  phone: 4057997666,
+  phone_number: 4057997666,
   website: "https://www.royal-bavaria.com/",
   online_ordering: false,
   image: "https://fastly.4sqi.net/img/general/600x600/N46yAlCI1R2LVr4I1s61OGNIgn-RMTxltkchUTeC6lY.jpg"
@@ -38,10 +38,66 @@ Restaurant.create(
   city: "Arlington",
   state: "TX",
   food_type: "Mexican",
-  phone: 6822597154,
-  website: ,
+  phone_number: 6822597154,
   online_ordering: true,
   image: "https://img-aws.ehowcdn.com/700x/www.onlyinyourstate.com/wp-content/uploads/2021/12/campo5.png"
 )
+
+Food.create(
+  food_name: "Pad Kra Pao",
+  restaurant_id: 1,
+  price: 14.00,
+  description:"Minced chicken, pork, tofu, or vegetables stir fried with chopped green beans, bell peppers, onions, and fresh basil leaves in a basil stir fry sauce. Served with a side of steamed white rice topped with a fried egg",
+  image: "https://menufyproduction.imgix.net/637943867641285015+865516.png?auto=compress,format&h=400&w=600&fit=max"
+)
+
+Food.create(
+  food_name: "Evil Jungle Curry",
+  restaurant_id: 1,
+  price: 13.00,
+  description: " Minced chicken (or fried tofu) with coconut milk, chili paste, and shredded cabbage. Garnished with chopped red & green bell peppers and cilantro.",
+  image: "https://menufyproduction.imgix.net/637942013954887514+864805.png?auto=compress,format&h=400&w=600&fit=max"
+)
+
+Food.create(
+  food_name: "Garlic Stir Fry",
+  restaurant_id: 1,
+  price: 12.00,
+  description: "Selected meat, tofu, or vegetables sautéed with garlic sauce, mushrooms, baby corn, carrots, red & green bell peppers, and onions.",
+  image: "https://menufyproduction.imgix.net/637942011546695538+864798.png?auto=compress,format&h=400&w=600&fit=max"
+)
+
+Food.create(
+  food_name: "Basil Stir Fry",
+  restaurant_id: 1,
+  price: 12.00,
+  description: "Selected meat, tofu, or vegetables stir-fried with fresh basil leaves, chili, red & green bell peppers, onions, and green beans.",
+  image: "https://menufyproduction.imgix.net/637942369087353221+864907.png?auto=compress,format&h=400&w=600&fit=max"  
+)
+
+Food.create(
+  food_name: "The ATW",
+  restaurant_id: 2,
+  price: 14.99,
+  description: "Hideaway Red Sauce, Mozzarella, Pepperoni, Sausage, Black Olives, Green Bell Pepper, Mushrooms, Red Onion",
+  image: "https://images.squarespace-cdn.com/content/v1/5cbf5a350cf57df7fb43982e/1556913782298-S4UDERUBM4R2BHTFVCS1/ATW.png?format=750w"
+)
+
+Food.create(
+  food_name: "Pepperonipalooza",
+  restaurant_id: 2,
+  price: 13.59,
+  description: "Red sauce, mozzarella, our traditional pepperoni, cup 'n' curl pepperoni, and deli pepperoni, topped with our Parmesan-herb shake.",
+  image: "https://images.squarespace-cdn.com/content/v1/5cbf5a350cf57df7fb43982e/1586206380263-0BR398YMMH9LV8RU896Y/Palooza.png?format=750w"
+)
+
+Food.create(
+  food_name: "Homemade Lasagna",
+  restaurant_id: 2,
+  price: 11.99,
+  description: "Made from scratch! Lasagna noodles layered with ricotta, Parmesan and mozzarella cheeses and our Italian sausage marinara sauce, topped with melted mozzarella!",
+  image: "https://assets-global.website-files.com/59b8855056b61b0001c8eaa8/5db072d25424c311d46a7717_hideaway%20-%20lasagna%201.jpg"
+)
+
 
 puts "✅ Done seeding!"
